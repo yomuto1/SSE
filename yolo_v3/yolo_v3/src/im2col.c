45f5d35d4aef9375f057875ad93bd4b728d4706d
yomuto1 <yomuto@gmail.com>
2018-11-30 23:05:58 +0800

    im2col optimization

--- yolo_v3/yolo_v3/src/im2col.c
+++ yolo_v3/yolo_v3/src/im2col.c
@@ -1,15 +1,4 @@
 #include "im2col.h"
-#include <stdio.h>
-float im2col_get_pixel(float *im, int height, int width, int channels,
-                        int row, int col, int channel, int pad)
-{
-    row -= pad;
-    col -= pad;
-
-    if (row < 0 || col < 0 ||
-        row >= height || col >= width) return 0;
-    return im[col + width*(row + height*channel)];
-}
 
 //From Berkeley Vision's Caffe!
 //https://github.com/BVLC/caffe/blob/master/LICENSE
@@ -18,21 +7,39 @@ void im2col_cpu(float* data_im,
      int ksize,  int stride, int pad, float* data_col) 
 {
     int c,h,w;
-    int height_col = (height + 2*pad - ksize) / stride + 1;
-    int width_col = (width + 2*pad - ksize) / stride + 1;
+    const int height_col = (height + 2*pad - ksize) / stride + 1;
+    const int width_col = (width + 2*pad - ksize) / stride + 1;
+    const int channels_col = channels * ksize * ksize;
+	int im_row, im_col;
+	float tmp_dat_f32;
 
-    int channels_col = channels * ksize * ksize;
-    for (c = 0; c < channels_col; ++c) {
-        int w_offset = c % ksize;
-        int h_offset = (c / ksize) % ksize;
-        int c_im = c / ksize / ksize;
-        for (h = 0; h < height_col; ++h) {
-            for (w = 0; w < width_col; ++w) {
-                int im_row = h_offset + h * stride;
-                int im_col = w_offset + w * stride;
-                int col_index = (c * height_col + h) * width_col + w;
-                data_col[col_index] = im2col_get_pixel(data_im, height, width, channels,
-                        im_row, im_col, c_im, pad);
+	for (c = 0; c < channels_col; ++c)
+	{
+        const int w_offset = c % ksize;
+        const int h_offset = (c / ksize) % ksize;
+        const int c_im = c / ksize / ksize;
+
+        for (h = 0; h < height_col; ++h)
+		{
+			float * __restrict p_tmp_data_col_f32 = &data_col[(c * height_col + h) * width_col];
+			const float * __restrict p_tmp_data_im_f32 = &data_im[width * ((h_offset + h * stride - pad) + height * c_im)];
+
+			im_row = h_offset + h * stride - pad;
+
+			for (w = 0; w < width_col; ++w)
+			{
+                im_col = w_offset + w * stride - pad;
+
+				if ((im_row >= 0) && (im_col >= 0) && (im_row < height) && (im_col < width))
+				{
+					tmp_dat_f32 = p_tmp_data_im_f32[im_col];
+				}
+				else
+				{
+					tmp_dat_f32 = 0;
+				}
+
+				*p_tmp_data_col_f32++ = tmp_dat_f32;
             }
         }
     }
